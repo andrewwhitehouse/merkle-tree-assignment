e3409935f304422fa6851446c51e524347066b5f
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-10-23 17:51:49 +0100

    Generate output file

--- src/merkle_tree/core.clj
+++ src/merkle_tree/core.clj
@@ -15,14 +15,26 @@
       :middle-left-merkle-proof (proof-fn txns middle-leaf-index)}))
   ([transactions] (calculate-root-and-proof transactions tree/compute-merkle-root tree/compute-merkle-proof)))
 
-(defn read-csv [file]
-  (with-open [reader (-> file io/resource io/reader)]
+(defn read-csv [resource]
+  (with-open [reader (-> resource io/resource io/reader)]
     (let [rows (doall (csv/read-csv reader))
           header (first rows)
           data (map (fn [row] (zipmap (map keyword header) row)) (rest rows))]
       data)))
 
+(defn format-transactions [row]
+  (str/join "," (row :transactions)))
+
+(defn write-csv [file data]
+  (with-open [writer (io/writer file)]
+    (csv/write-csv
+      writer
+      (concat
+        [["transactions" "merkleRoot" "middleLeafMerkleProof"]]
+        (map (juxt format-transactions :merkle-root :middle-left-merkle-proof) data))
+      :quote \")))
+
 (defn -main []
   (let [data (read-csv "original_data.csv")]
-    (println data)
-    (pprint (map #(calculate-root-and-proof (:transactions %)) data))))
+    (println "Writing data.csv ...")
+    (write-csv "data.csv" (map #(calculate-root-and-proof (:transactions %)) data))))
